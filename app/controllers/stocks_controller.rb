2dd84e449e0635184d7bb08b1ae9a1954d803fb2
Navin <navinvarma@yahoo.com>
2019-01-20 16:02:56 +0530

    Converted stock lookup form into an AJAX powered one

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,18 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio' 
+    if params[:stock] != ''
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        flash[:danger] = 'You have entered an incorrect symbol'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = 'You have not provided a symbol to search for'
+      redirect_to my_portfolio_path
+    end
   end
 end
