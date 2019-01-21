d8d4882379efb5c9c2f98d2d22010ee0b0e12309
Navin <navinvarma@yahoo.com>
2019-01-21 17:42:54 +0530

    Added UserStocks model and relationship between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,6 @@
 class Stock < ApplicationRecord
-
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   def self.new_from_lookup(ticker_symbol)
     begin
       look_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -7,6 +8,10 @@ class Stock < ApplicationRecord
     rescue Exception => e
       return nil
     end
+  end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
 end
